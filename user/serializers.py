178442839140b53dc5ea1ec30a34f2ff9311dee0
Vlad Larin <lordgadyka71@gmail.com>
2024-01-06 15:47:13 +0200

    Add user.serializers module with UserSerializer

---
+++ user/serializers.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "email", "password", "is_staff")
+        read_only_fields = ("is_staff",)
+        extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
