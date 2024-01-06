2fb048403dbe5a86eda8b83a9efb25286ed14e1f
Vlad Larin <lordgadyka71@gmail.com>
2024-01-06 17:21:28 +0200

    Add book endpoints in urls

--- drf_library/urls.py
+++ drf_library/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/user/", include("user.urls", namespace="user")),
+    path("api/users/", include("user.urls", namespace="user")),
+    path("api/", include("book.urls", namespace="book")),
 ]
