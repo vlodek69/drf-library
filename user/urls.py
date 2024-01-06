90fcb8feb8288d0092b07e0c2295568430b529bd
Vlad Larin <lordgadyka71@gmail.com>
2024-01-06 15:59:26 +0200

    Add ManageUserView in user.urls

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
 
 app_name = "user"
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path("", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
