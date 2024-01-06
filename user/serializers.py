c8343c301dfb52ee4ddfb097a314ea5cbd08c31a
Vlad Larin <lordgadyka71@gmail.com>
2024-01-06 15:47:37 +0200

    Add create method in UserSerializer

--- user/serializers.py
+++ user/serializers.py
@@ -8,3 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("is_staff",)
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
+
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
