ef5dc278b968467340a5e31804b42d2a5ebb7bf8
Vlad Larin <lordgadyka71@gmail.com>
2024-01-12 17:09:06 +0200

    Update send_telegram_message in telegram_utils.py to use async

--- borrowing/telegram_utils.py
+++ borrowing/telegram_utils.py
@@ -1,6 +1,6 @@
 from telegram import Bot
 
 
-def send_telegram_message(bot_token, chat_id, message):
+async def send_telegram_message(bot_token, chat_id, message):
     bot = Bot(token=bot_token)
-    bot.send_message(chat_id=chat_id, text=message)
+    await bot.send_message(chat_id=chat_id, text=message)
