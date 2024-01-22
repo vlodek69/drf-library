6b31f5d974d5af96f7d77c6fb78414db9f5c70c6
Vlad Larin <lordgadyka71@gmail.com>
2024-01-22 16:22:13 +0200

    DRY use of serializers in borrowing view

--- borrowing/views.py
+++ borrowing/views.py
@@ -72,11 +72,9 @@ class BorrowingViewSet(
         borrowing = self.get_object()
         serializer = self.get_serializer(borrowing, data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @extend_schema(
         parameters=[
